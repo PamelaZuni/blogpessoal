e0e21d00a15b096fe5864fe37621061bccd0bb06
Pamela Rodrigues <p.pml@hotmail.com>
2024-04-25 17:25:22 -0300

    added the user photo

--- src/usuario/services/usuario.service.ts
+++ src/usuario/services/usuario.service.ts
@@ -48,6 +48,10 @@ export class UsuarioService {
     let buscaUsuario = await this.findByUsuario(usuario.usuario);
 
     if (!buscaUsuario) {
+
+      if (!usuario.foto)
+        usuario.foto = 'https://i.imgur.com/Sk5SjWE.jpg'
+      
       usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
       return await this.usuarioRepository.save(usuario);
     }
@@ -67,6 +71,9 @@ export class UsuarioService {
         'Usuário (e-mail) já Cadastrado!',
         HttpStatus.BAD_REQUEST,
       );
+
+      if (!usuario.foto)
+        usuario.foto = 'https://i.imgur.com/Sk5SjWE.jpg'
 
     usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
     return await this.usuarioRepository.save(usuario);
