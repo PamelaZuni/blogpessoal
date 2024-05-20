24f7168edac803f1f6826dc8564b106d595faabe
Pamela Rodrigues <p.pml@hotmail.com>
2024-05-20 17:46:09 -0300

    Added the comment description into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,14 @@ import { UsuarioModule } from './usuario/entities/usuario.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
-// import { DevService } from './data/services/dev.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
-      // useClass: DevService,
+      useClass: ProdService, // uncomment when doing git commit. It need this line uncomment to run the render
+      // useClass: DevService, //uncomment this line when running local, which means to run on local computer
       imports: [ConfigModule],
     }),
     PostagemModule,
